bdafec1ceb3e3a593484904e69f89a46d62eb8a4
jmjcoding <jmjcoding@icloud.com>
2024-08-19 12:38:30 -0500

    Create Footer folder, with Footer.tsx, .css, .stories.tsx, import vectors per figma, import footer into App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 
 import { Header } from "../Header/Header";
+import { Footer } from "../Footer/Footer";
 function App () {
 
     return (
+        <div className="app">
         <Header/>
+        <Footer/>
+        </div>
     )
 }
 
---
+++ src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,19 @@
+import {Meta, StoryFn} from '@storybook/react';
+import {BrowserRouter} from 'react-router-dom';
+import { Footer } from './Footer';
+
+export default {
+    title: 'components/Footer',
+    component: Footer,
+} as Meta;
+
+const Template: StoryFn = (args) => {
+    return (
+    <BrowserRouter>
+    <Footer {...args}/>
+    </BrowserRouter>
+    );
+}
+
+export const Default = Template.bind({});
+Default.args = {}
