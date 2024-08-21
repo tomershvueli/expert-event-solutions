26d1ccaadf1ed165f41ea0ab30da035787bfb22f
jmjcoding <jmjcoding@icloud.com>
2024-08-21 13:55:15 -0500

    Created Modal, Modal.stories, ModalContext, ModalHook, wrapped <App /> in <ModalProvider/>

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,25 @@
 import "./App.css";
-
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
-function App () {
+import { Modal } from "../Modal/Modal";
+import { useModal } from "../ModalContext/ModalHook";
+function App() {
+  const modalName = "modal"; // Unique identifier for the modal
+  // import closeModal from useModal if you want a button to also close the modal
+  const { isOpen, openModal } = useModal(modalName); // Use the hook to control the modal
 
-    return (
-        <div className="app">
-        <Header/>
-        <Footer/>
-        </div>
-    )
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+      <button onClick={openModal}>Open Modal</button>
+      {isOpen && (
+        <Modal name={modalName}>
+          <div></div>
+        </Modal>
+      )}
+    </div>
+  );
 }
 
 export default App;
