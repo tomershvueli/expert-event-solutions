f52bded5b049a091205e5f5e6ac952f111a97203
Marcelluki <marcell.rangel29@gmail.com>
2024-08-23 19:24:33 -0700

    feat: reusable button component

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./global/default.css";
+import { Button } from "./components/Button";
 
 const client = new ApolloClient({
   uri: "http://localhost:8080/api/graphql",
@@ -13,9 +14,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<div>Home Route</div>} />
-        </Routes>
+        
+        <Button submit={console.log} buttonText={"Contact Us!"}/>
       </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
