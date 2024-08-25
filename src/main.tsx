869b1e8d771825e5572c7c953a7f70046fcb6585
Marcelluki <marcell.rangel29@gmail.com>
2024-08-25 15:05:35 -0700

    attemped to make button more reusable also added storybook

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./global/default.css";
-import { Button } from "./components/Button";
+import { Button } from "./components/Button/Button";
 
 const client = new ApolloClient({
   uri: "http://localhost:8080/api/graphql",
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ApolloProvider client={client}>
       <BrowserRouter>
         
-        <Button submit={console.log} buttonText={"Contact Us!"}/>
+        <Button submit={console.log} />
       </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
