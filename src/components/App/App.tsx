4e29a72720bbd5678f37bf9731e18165cadf2747
jmjcoding <jmjcoding@icloud.com>
2024-08-26 15:48:16 -0500

    remove header from this branch

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,12 @@
 import "./App.css";
 
-import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
-function App () {
-
-    return (
-        <div className="app">
-        <Header/>
-        <Footer/>
-        </div>
-    )
+function App() {
+  return (
+    <div className="app">
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
