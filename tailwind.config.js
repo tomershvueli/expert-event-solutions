0da08dcb833a4e08ca2d134fd0ec8ad039d363ae
RuvenPinkhasov <rubi00010@gmail.com>
2024-08-27 19:15:19 -0400

    fix: combined the two content arrays into one

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@ import HeadlessTailwindPlugin from "@headlessui/tailwindcss";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
-  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  content: [
+    "./index.html",
+    "./public/**/*.html",
+    "./src/**/*.{html,js,jsx,ts,tsx,vue}",
+  ],
   theme: {
     screens: { sm: "340px" },
     extend: {
