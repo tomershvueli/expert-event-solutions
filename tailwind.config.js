1f812c1370285d22666c8b20e9583fbdbc2a32d0
RuvenPinkhasov <rubi00010@gmail.com>
2024-08-27 16:34:56 -0400

    fix: removed unneeded import and deprecated  options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
-import { faWheatAlt } from "@fortawesome/free-solid-svg-icons";
 import HeadlessTailwindPlugin from "@headlessui/tailwindcss";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
-  mode: "jit",
-  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   theme: {
     screens: { sm: "340px" },
     extend: {
