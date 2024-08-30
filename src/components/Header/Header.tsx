527fd21da04a9e1207cf1b6c62e7096788d281d0
jmjcoding <jmjcoding@icloud.com>
2024-08-30 09:44:56 -0500

    update className to tailwind css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,40 @@
 import "./Header.css";
 import EES from "../../images/EES.svg";
 import { Link } from "react-router-dom";
-export function Header () {
-    return (
-        <div className="header"> 
-            <Link to="/">
-            {/* run npm run storybook:node16 */}
-             {/* http://localhost:6006/iframe.html?id=components-header--default */}
-            <img className="header__logo" src={EES} alt="EES Logo"></img>
-            </Link>
-            <ol className="header__navbar">
-                <li className="header__navbar-item" onClick={() => console.log('clicked about us')}>About us</li>
-                <li className="header__navbar-item" onClick={() => console.log('clicked renting equipment')}>Renting equipment</li>
-                <li className="header__navbar-item" onClick={() => console.log('clicked services')}>Staffing services</li>
-                <li className="header__navbar-item" onClick={() => console.log('clicked contact')}>Contact</li>
-            </ol>
-        </div>
-    )
+export function Header() {
+  return (
+    <div className="flex justify-between items-center w-full h-[74px] px-[128px] py-[80px] bg-wheat">
+      <Link to="/">
+        {/* run npm run storybook:node16 */}
+        {/* http://localhost:6006/iframe.html?id=components-header--default */}
+        <img src={EES} alt="EES Logo"></img>
+      </Link>
+      <ol className="flex justify-between items-center p-0 m-0 gap-[20px]">
+        <li
+          className="font-text text-[20px] font-medium leading-[18px] text-center cursor-pointer"
+          onClick={() => console.log("clicked about us")}
+        >
+          About us
+        </li>
+        <li
+          className="font-text text-[20px] font-medium leading-[18px] text-center cursor-pointer"
+          onClick={() => console.log("clicked renting equipment")}
+        >
+          Renting equipment
+        </li>
+        <li
+          className="font-text text-[20px] font-medium leading-[18px] text-center cursor-pointer"
+          onClick={() => console.log("clicked services")}
+        >
+          Staffing services
+        </li>
+        <li
+          className="font-text text-[20px] font-medium leading-[18px] text-center cursor-pointer"
+          onClick={() => console.log("clicked contact")}
+        >
+          Contact
+        </li>
+      </ol>
+    </div>
+  );
 }
