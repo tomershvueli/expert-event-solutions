892037f1c6072403cc2fac9d6d1dead0c48bb6d6
raymondafan <raymond.afan@icloud.com>
2024-08-30 18:48:53 -0700

    feat: pasting checkbox and textarea frm old branch

--- src/components/CheckBoxInput/CheckBoxInput.tsx
+++ src/components/CheckBoxInput/CheckBoxInput.tsx
@@ -1,7 +1,9 @@
 import "./CheckboxInput.css";
 import React from "react";
+import { useState } from "react";
 import { useFormContext } from "react-hook-form";
-
+import checkBox from "../../images/checkbox.svg";
+import check from "../../images/check.svg";
 //useFormContext is a hook from the react-hook-form library, which helps manage forms in React applications.
 interface CheckboxInputProps {
   name: string;
@@ -17,24 +19,33 @@ const CheckboxInput: React.FC<CheckboxInputProps> = ({
 }) => {
   //React.FC stands for "Functional Component" and is a type that represents a functional component in React.
   //The component expects to receive the name, label, and error props, which are destructured from the props object.
-
+  const [isClicked, setIsClicked] = useState(false);
   const { register } = useFormContext();
   //The useFormContext hook returns an object with a register property, which is used to register the checkbox input with the form.
+  const handleisClicked = () => {
+    setIsClicked(!isClicked);
+  };
   return (
     <div className="checkbox-input">
       <label className="checkbox-label">
-        <input
-          type="checkbox"
+        <img
+          src={checkBox}
+          onClick={handleisClicked}
           className="checkbox-input-field"
           {...register(name, {
             required: "You must agree to the terms",
           })}
           //The "register" function is called with the "name" prop as an argument. The "register" function returns an object with a required property.
         />
+        {isClicked && (
+          <img className="check" src={check} onClick={handleisClicked} />
+        )}
         {label}
         {/*The "label" prop is passed as a child element of the "label" element. */}
       </label>
-      {error && <span className="checkbox-error">{error.message}</span>}
+      {error && !isClicked ? (
+        <span className="checkbox-error">{error.message}</span>
+      ) : null}
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App/App";
 import "./global/default.css";
 
 const client = new ApolloClient({
@@ -13,9 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<div>Home Route</div>} />
-        </Routes>
+        <App />
       </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
