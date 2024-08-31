6e6611bf5bf0acd30a054854d93ba5cd512c9250
jmjcoding <jmjcoding@icloud.com>
2024-08-31 10:35:30 -0500

    Revert "feat: completed merge"
    This reverts commit 73d2b5e492ee0ce0aa200998e78c69f2588b454f.

---
+++ .storybook/main.ts
@@ -0,0 +1,23 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+<<<<<<< HEAD
+=======
+  docs: {
+    autodocs: "tag",
+  },
+>>>>>>> 39d682af1468c7acb6e39d109e27f36ad20164fe
+};
+export default config;
