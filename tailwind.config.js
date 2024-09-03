970ab7d26ccb5c300c4e6916ffd0892bdd48094a
jmjcoding <jmjcoding@icloud.com>
2024-09-03 16:03:31 -0500

    revert tailwind.config.js file back to old version

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,7 @@ export default {
     "./src/**/*.{html,js,jsx,ts,tsx,vue}",
   ],
   theme: {
-    screens: {
-      xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
-      lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
-      md: { max: "767px" },
-      // => @media (max-width: 767px) { ... }
-      sm: { max: "639px" },
-      // => @media (max-width: 639px) { ... }
-    },
-
+    screens: { sm: "340px" },
     extend: {
       colors: {
         black: "#111",
