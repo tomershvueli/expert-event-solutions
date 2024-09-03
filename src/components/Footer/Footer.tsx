80186093e3909af702799b49bc0426857df75800
jmjcoding <jmjcoding@icloud.com>
2024-09-03 13:20:14 -0500

    update tailwind to use max width instead of min width, add styles to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,48 +4,48 @@ import linkedin from "../../images/linkedin.svg";
 
 export const Footer = () => {
   return (
-    <footer className="flex w-full bg-wheat p-[80px] gap-[40px]">
-      <div className="flex flex-col gap-[40px]">
+    <footer className="flex w-full bg-wheat p-[80px] gap-[40px] sm:flex-col sm:p-[16px] sm:gap-[26px]">
+      <div className="flex flex-col gap-[40px] sm:gap-[26px]">
         <div className="flex flex-col gap-[24px]">
-          <p className="font-text text-[20px] font-normal leading-[26px] tracking-[-0.02em] text-left">
+          <p className="font-text text-[20px] font-normal leading-[26px] tracking-[-0.02em] text-left sm:font-text sm:text-[20px] sm:font-normal sm:leading-[26px] sm:tracking-[-0.02em] sm:text-center ">
             Expert Event Solutions
           </p>
           <p
-            className="font-text text-[20px] font-normal leading-[26px] tracking-[-0.02em] text-left cursor-pointer"
+            className="font-text text-[20px] font-normal leading-[26px] tracking-[-0.02em] text-left sm:font-text sm:text-[20px] sm:font-normal sm:leading-[26px] sm:tracking-[-0.02em] sm:text-center  cursor-pointer"
             onClick={() => console.log("clicked address")}
           >
             260 Daytona Blvd, Daytona <br /> Beach, Florida 32114, United <br />{" "}
             States
           </p>
           <p
-            className="font-text text-[20px] font-normal leading-[26px] tracking-[-0.02em] text-left cursor-pointer"
+            className="font-text text-[20px] font-normal leading-[26px] tracking-[-0.02em] text-left sm:font-text sm:text-[20px] sm:font-normal sm:leading-[26px] sm:tracking-[-0.02em] sm:text-center  cursor-pointer"
             onClick={() => console.log("clicked email")}
           >
             Info@experteventsolutions.com
           </p>
         </div>
 
-        <p className="font-text text-[20px] font-normal leading-[26px] tracking-[-0.02em] text-left">
+        <p className="font-text text-[20px] font-normal leading-[26px] tracking-[-0.02em] text-left sm:font-text sm:text-[20px] sm:font-normal sm:leading-[26px] sm:tracking-[-0.02em] sm:text-center ">
           Copyright © 2022. Expert Event Solutions - All rights reserved.
         </p>
       </div>
 
       <div className="flex flex-col gap-[24px]">
         <p
-          className="font-text text-[20px] font-normal leading-[26px] tracking-[-0.02em] text-left cursor-pointer"
+          className="font-text sm:hidden"
           onClick={() => console.log("clicked contacts")}
         >
           Contacts
         </p>
         <p
-          className="font-text text-[20px] font-normal leading-[26px] tracking-[-0.02em] text-left cursor-pointer"
+          className="font-text sm:hidden"
           onClick={() => console.log("clicked rental conditions")}
         >
           Rental Conditions
         </p>
 
-        <div className="flex flex-col gap-[10px]">
-          <p className="font-text text-[20px] font-normal leading-[26px] tracking-[-0.02em] text-left">
+        <div className="flex flex-wrap flex-col gap-[10px] sm:content-center">
+          <p className="font-text text-[20px] font-normal leading-[26px] tracking-[-0.02em] text-left sm:font-text sm:text-[20px] sm:font-normal sm:leading-[26px] sm:tracking-[-0.02em] sm:text-center ">
             Social Media
           </p>
           <div className="flex flex-row gap-[10px] cursor-pointer">
