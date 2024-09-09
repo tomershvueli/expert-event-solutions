c572d9eb50884aa3c8076aae2babc3a0c28df4b2
raymondafan <raymond.afan@icloud.com>
2024-09-09 14:00:55 -0700

    feat: editing the form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useForm, FormProvider } from "react-hook-form";
-import submitButton from "../../images/submitbutton.svg";
+// import submitButton from "../../images/submitbutton.svg";
 
 export type FormValues = {
   description: string;
@@ -34,42 +34,17 @@ const Form: React.FC<FormProps> & {
         <div className="flex flex-col justify-center ">
           {children}
 
-          <button type="submit" className="flex mt-[42px] justify-center">
+          {/* <button type="submit" className="flex mt-[42px] justify-center">
             <img
               className="w-[253px] h-[71px] opacity-100 hover:opacity-80 transition-opacity duration-300"
               src={submitButton}
               alt="Submit button"
             />
-          </button>
+          </button> */}
         </div>
       </form>
     </FormProvider>
   );
 };
-
-// interface TextAreaInputProps {
-//   name: keyof FormValues; // Restrict name to keys of FormValues
-//   placeholder?: string;
-//   error?: { message: string };
-// }
-// const TextAreaInput: React.FC<TextAreaInputProps> = ({ name, placeholder }) => {
-//   const {
-//     register,
-//     formState: { errors },
-//   } = useFormContext<FormValues>();
-//   const errorMessage = errors[name]?.message as string | undefined;
-//   return (
-//     <div className="flex flex-col items-center">
-//       <textarea
-//         className="w-full h-[167px] rounded-[12px] p-3.5 bg-[#E9E8D5] mb-2.5 box-border
-//       text-base font-roboto leading-[19.2px] resize-none"
-//         {...register(name, { required: "This field is required" })}
-//         placeholder={placeholder}
-//       />
-//       {errorMessage && <p className="text-red">{errorMessage}</p>}
-//     </div>
-//   );
-// };
-// Form.TextAreaInput = TextAreaInput;
 
 export default Form;
