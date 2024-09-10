7138cc1778336146d7f5107f0151c2db0762e420
jmjcoding <jmjcoding@icloud.com>
2024-09-10 13:35:18 -0500

    update header to use be responsive with mobile devices

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,16 +4,15 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export function Header() {
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
   const [showMenuIcon, setShowMenuIcon] = useState<boolean>(false);
   const [showMenu, setShowMenu] = useState<boolean>(false);
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth < 800) {
-        setShowMenuIcon(true);
-      } else {
-        setShowMenuIcon(false);
-      }
+      const width = window.innerWidth;
+      setWindowWidth(width); // Track window width
+      setShowMenuIcon(width < 800 ? true : false);
     };
     handleResize();
     window.addEventListener("resize", handleResize);
@@ -26,16 +25,26 @@ export function Header() {
     <>
       <div
         className={`flex justify-between items-center w-full h-[74px] px-[128px] py-[80px] bg-wheat
-          ${showMenu ? "border-x-red border-x-4" : ""} `}
+          border-x-4 border-transparent
+          ${showMenu && windowWidth < 800 ? "border-x-red border-x-4" : ""}
+          ${windowWidth < 800 ? "px-[8px] py-[8px]" : ""}`}
       >
         <Link to="/">
           {/* showMenu then add className with border-x-red */}
-          <img src={EES} alt="EES Logo"></img>
+          <img
+            src={EES}
+            alt="EES Logo"
+            className={`${ windowWidth < 800 ? "w-[51px] mx-[8.4px] my-[6.3px]" : "" }`}
+          ></img>
         </Link>
         <ul className="flex justify-between items-center p-0 m-0 gap-[20px]">
           {showMenuIcon ? (
             <button onClick={() => setShowMenu(!showMenu)}>
-              <img src={MenuIcon} alt="Menu Icon"></img>
+              <img
+                src={MenuIcon}
+                alt="Menu Icon"
+                className="px-[12px] py-[23px]"
+              ></img>
             </button>
           ) : (
             <>
@@ -71,41 +80,43 @@ export function Header() {
           )}
         </ul>
       </div>
-      {showMenu && (
+      {showMenu && windowWidth < 800 ? (
         <div
-          className="flex flex-col gap-[20px] pt-[103px] pb-[190px] px-[122px] bg-wheat border-b-4
-            border-x-4 border-red rounded-b-[80px]"
+          className="flex flex-col gap-[20px] bg-wheat border-b-4 border-x-4 border-red
+            rounded-b-[80px]"
         >
-          <a
-            className="font-text text-paragraph-sm font-medium leading-[18px] text-center
-              cursor-pointer"
-            onClick={() => console.log("clicked about us")}
-          >
-            About us
-          </a>
-          <a
-            className="font-text text-paragraph-sm font-medium leading-[18px] text-center
-              cursor-pointer"
-            onClick={() => console.log("clicked renting equipment")}
-          >
-            Renting equipment
-          </a>
-          <a
-            className="font-text text-paragraph-sm font-medium leading-[18px] text-center
-              cursor-pointer"
-            onClick={() => console.log("clicked services")}
-          >
-            Staffing services
-          </a>
-          <a
-            className="font-text text-paragraph-sm font-medium leading-[18px] text-center
-              cursor-pointer"
-            onClick={() => console.log("clicked contact")}
-          >
-            Contact
-          </a>
+          <div className="flex flex-col gap-[20px] pt-[100px] pb-[190px]">
+            <a
+              className="font-text text-paragraph-sm font-medium leading-[18px] text-center
+                cursor-pointer text-red"
+              onClick={() => console.log("clicked about us")}
+            >
+              About us
+            </a>
+            <a
+              className="font-text text-paragraph-sm font-medium leading-[18px] text-center
+                cursor-pointer text-red"
+              onClick={() => console.log("clicked renting equipment")}
+            >
+              Renting equipment
+            </a>
+            <a
+              className="font-text text-paragraph-sm font-medium leading-[18px] text-center
+                cursor-pointer text-red"
+              onClick={() => console.log("clicked services")}
+            >
+              Staffing services
+            </a>
+            <a
+              className="font-text text-paragraph-sm font-medium leading-[18px] text-center
+                cursor-pointer text-red"
+              onClick={() => console.log("clicked contact")}
+            >
+              Contact
+            </a>
+          </div>
         </div>
-      )}
+      ) : null}
     </>
   );
 }
