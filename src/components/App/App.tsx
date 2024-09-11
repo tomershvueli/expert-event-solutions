9041a572d1d825c3fdce723c23d26d2ef6805fe7
raymondafan <raymond.afan@icloud.com>
2024-09-10 17:04:31 -0700

    feat: adding development environment rendering to test

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import Form from "../Form/Form"; // Ensure this path is correct
 import CheckboxInput from "../CheckboxInput/CheckboxInput"; // Ensure this path is correct
 
 const App: React.FC = () => {
+  const isDevelopment = process.env.NODE_ENV === "development";
   return (
     <div className="flex justify-center">
       <Routes>
@@ -11,17 +12,23 @@ const App: React.FC = () => {
           path="/"
           element={
             <div className="w-full max-w-[467px] mx-auto">
-              {/* Wrapper for Form: Centers the form with a max width */}
-              <Form
-                submit={(data) =>
-                  console.log("Form submitted with data:", data)
-                }
-              >
+              {isDevelopment ? (
+                <Form
+                  submit={(data) =>
+                    console.log("Form submitted with data:", data)
+                  }
+                >
+                  <CheckboxInput
+                    name="agreeToTerms"
+                    label="I agree to receive automated calls or text messages from Expert Event Solutions LLC in response to my inquiry. Msg & data rates may apply."
+                  />
+                </Form>
+              ) : (
                 <CheckboxInput
                   name="agreeToTerms"
                   label="I agree to receive automated calls or text messages from Expert Event Solutions LLC in response to my inquiry. Msg & data rates may apply."
                 />
-              </Form>
+              )}
             </div>
           }
         />
