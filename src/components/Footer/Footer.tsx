7c22f1c3c0ed3326232cb4baadc6510b8c593d6f
jmjcoding <jmjcoding@icloud.com>
2024-09-11 14:50:59 -0500

    create new branch after merging local main to remote main for footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,76 @@
+import facebook from "../../images/facebook.svg";
+import instagram from "../../images/instagram.svg";
+import linkedin from "../../images/linkedin.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="flex w-full flex-col bg-wheat p-[80px] gap-[40px] sm:flex-col sm:p-[16px] sm:gap-[26px]">
+      <div className="flex flex-row gap-[287px] sm:gap-[26px] sm:flex-col">
+        <div className="flex flex-col gap-[40px] sm:gap-[26px]">
+          <div className="flex flex-col gap-[24px]">
+            <p className="font-text text-paragraph-sm font-normal leading-[26px] tracking-[-0.02em] text-left sm:tracking-[-0.02em] sm:text-center ">
+              Expert Event Solutions
+            </p>
+            <a
+              className="font-text text-paragraph-sm font-normal leading-[26px] tracking-[-0.02em] text-left  sm:tracking-[-0.02em] sm:text-center  cursor-pointer"
+              onClick={() => console.log("clicked address")}
+            >
+              260 Daytona Blvd, Daytona <br /> Beach, Florida 32114, United{" "}
+              <br /> States
+            </a>
+            <p
+              className="font-text text-paragraph-sm font-normal leading-[26px] tracking-[-0.02em] text-left  sm:tracking-[-0.02em] sm:text-center  cursor-pointer"
+              onClick={() => console.log("clicked email")}
+            >
+              Info@experteventsolutions.com
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-[24px]">
+          <p
+            className="font-text text-paragraph-sm sm:hidden"
+            onClick={() => console.log("clicked contacts")}
+          >
+            Contacts
+          </p>
+          <p
+            className="font-text text-paragraph-sm sm:hidden"
+            onClick={() => console.log("clicked rental conditions")}
+          >
+            Rental Conditions
+          </p>
+
+          <div className="flex flex-wrap flex-col gap-[10px] sm:content-center">
+            <p className="font-text text-paragraph-sm font-normal leading-[26px] tracking-[-0.02em] text-left  sm:tracking-[-0.02em] sm:text-center ">
+              Social Media
+            </p>
+            <div className="flex flex-row gap-[10px] cursor-pointer">
+              <img
+                src={instagram}
+                onClick={() => console.log("clicked instagram")}
+                alt="Instagram"
+              />
+              <img
+                src={facebook}
+                onClick={() => console.log("clicked facebook")}
+                alt="Facebook"
+              />
+              <img
+                src={linkedin}
+                onClick={() => console.log("clicked linkedin")}
+                alt="LinkedIn"
+              />
+            </div>
+          </div>
+          {/*  */}
+        </div>
+      </div>
+
+      <p className="font-text text-paragraph-sm font-normal leading-[26px] tracking-[-0.02em] text-left sm:font-text sm:tracking-[-0.02em] sm:text-center ">
+        Copyright © 2022. Expert Event Solutions - All rights reserved.
+      </p>
+      {/*  */}
+    </footer>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./global/default.css";
+import { Footer } from "./components/Footer/Footer";
 
 const client = new ApolloClient({
   uri: "http://localhost:8080/api/graphql",
@@ -14,7 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ApolloProvider client={client}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div>Home Route</div>} />
+          <Route path="/" element={<Footer />} />
         </Routes>
       </BrowserRouter>
     </ApolloProvider>
