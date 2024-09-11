7649bed15e8c746d3db26829f45269e722e735a0
jmjcoding <jmjcoding@icloud.com>
2024-09-11 12:55:47 -0500

    create staffing services array and style it for wider screen widths

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -94,21 +94,90 @@ function App() {
           },
         ];
 
+  const staffingItems =
+    windowWidth > 800
+      ? [
+          {
+            title: (
+              <>
+                1.
+                <br /> Planning the event, <br /> managing everything <br />
+                from logistics to decor
+              </>
+            ),
+            description: (
+              <>
+                to ensure a seamless <br /> and memorable experience.
+              </>
+            ),
+          },
+          {
+            title: (
+              <>
+                2.
+                <br /> Post-event support <br /> to ensure everything <br />{" "}
+                wraps up smoothly
+              </>
+            ),
+            description: (
+              <>
+                Our commitment doesn't end <br /> when the event does.
+              </>
+            ),
+          },
+          {
+            title: (
+              <>
+                3.
+                <br />
+                We tailor every event <br /> to your theme <br /> or request
+              </>
+            ),
+            description: (
+              <>
+                We pride ourselves on our individual <br /> approach to event
+                organizing
+              </>
+            ),
+          },
+        ]
+      : [];
+
   return (
-    <HorizontalList
-      containerClassName="bg-wheat"
-      unorderedListClassName="flex justify-between h-[260px] sm:flex-col sm:h-[100%] sm:flex-col sm:justify-center sm:items-center"
-    >
-      {HomePageItems.map((item) => (
-        <HorizontalList.Item
-          title={item.title}
-          description={item.description}
-          listClassName="flex flex-col w-[296px] text-center text-black tracking-[-0.02em] py-[24px] px-[24px] gap-[24px] sm:px-[12px] sm:gap-[12px]"
-          titleClassName="font-merriweather text-section-subtext text-lightGingerFlower leading-[38.4px]"
-          descriptionClassName="font-roboto text-[22px] text-black leading-[26.4px] sm:text-paragraph-sm "
-        />
-      ))}
-    </HorizontalList>
+    <div>
+      {/* Home Page List */}
+      <HorizontalList
+        containerClassName="bg-wheat"
+        unorderedListClassName="flex justify-between h-[260px] sm:flex-col sm:h-[100%] sm:flex-col sm:justify-center sm:items-center"
+      >
+        {HomePageItems.map((item) => (
+          <HorizontalList.Item
+            title={item.title}
+            description={item.description}
+            listClassName="flex flex-col w-[296px] 
+            text-center text-black tracking-[-0.02em] py-[24px] px-[24px] gap-[24px] sm:px-[12px] sm:gap-[12px]"
+            titleClassName="font-merriweather text-section-subtext text-lightGingerFlower leading-[38.4px]"
+            descriptionClassName="font-roboto text-[22px] text-black leading-[26.4px] sm:text-paragraph-sm"
+          />
+        ))}
+      </HorizontalList>
+      {/* Staffing Services List*/}
+      <HorizontalList
+        containerClassName="bg-wheat"
+        unorderedListClassName="bg-lightGingerFlower flex  justify-around h-[460px] py-[80px] px-[24px] rounded-[100px] sm:flex-col sm:h-[100%] sm:flex-col sm:justify-center sm:items-center"
+      >
+        {staffingItems.map((item) => (
+          <HorizontalList.Item
+            title={item.title}
+            description={item.description}
+            listClassName=" flex flex-col
+            text-center tracking-[-0.02em] py-[24px] px-[24px] gap-[24px] sm:px-[12px] sm:gap-[12px]"
+            titleClassName="font-merriweather text-section-subtext text-wheat leading-[38.4px]"
+            descriptionClassName="font-roboto text-[22px] text-wheat leading-[26.4px] sm:text-paragraph-sm"
+          />
+        ))}
+      </HorizontalList>
+    </div>
   );
 }
 
