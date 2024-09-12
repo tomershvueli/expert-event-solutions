57b7badfc4f5d22606faf64960e1a038da333df6
jmjcoding <jmjcoding@icloud.com>
2024-09-12 12:17:13 -0500

    update header to use remote main, update styles to reflect most recent tailwind changes in main

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./global/default.css";
-
+import { Header } from "./components/Header/Header";
 const client = new ApolloClient({
   uri: "http://localhost:8080/api/graphql",
   cache: new InMemoryCache(),
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ApolloProvider client={client}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div>Home Route</div>} />
+          <Route path="/" element={<Header />} />
         </Routes>
       </BrowserRouter>
     </ApolloProvider>
