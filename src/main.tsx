39db543d7333b8486ce582d4805310aa07549891
Tomer Shvueli <tomer@shvueli.com>
2024-09-15 10:44:40 -0400

    fix: newline

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./global/default.css";
+
 const client = new ApolloClient({
   uri: "http://localhost:8080/api/graphql",
   cache: new InMemoryCache(),
