98af4c079f1ce6afed8671403b03ac24e2b3aa3e
Marcelluki <marcell.rangel29@gmail.com>
2024-09-15 16:11:47 -0700

    feat: created rental equipment component

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Routes>
           <Route path="/" element={<div>Home Route</div>} />
         </Routes>
+        
       </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
