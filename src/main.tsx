725977d06c731b0130a4d70e1c3898c1ce93bd84
Tomer Shvueli <tomer@shvueli.com>
2024-09-15 10:43:44 -0400

    fix: revert src/main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./global/default.css";
-import { Header } from "./components/Header/Header";
 const client = new ApolloClient({
   uri: "http://localhost:8080/api/graphql",
   cache: new InMemoryCache(),
@@ -14,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ApolloProvider client={client}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Header />} />
+          <Route path="/" element={<div>Home Route</div>} />
         </Routes>
       </BrowserRouter>
     </ApolloProvider>
