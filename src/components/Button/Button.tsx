98f99bedfe60942f59ecbc12eb06e39760ed4ea1
RuvenPinkhasov <rubi00010@gmail.com>
2024-09-15 17:53:26 -0400

    fix: fixed colors to match tailwind config

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -12,17 +12,17 @@ export const Button = ({ submit, buttonText }: ButtonProps) => {
     <div className="modal__form-submit">
       <button
         // modal__form-submit__button
-        className="relative w-60 h-16 rounded-2xl border-4 border-[#cf554e] bg-[#cf554e]"
+        className="relative w-60 h-16 rounded-2xl border-4 border-lightGingerFlower bg-lightGingerFlower"
         type="submit"
         onClick={submit}
       >
         {/* modal__form-submit__text */}
-        <p className="font-title text-white font-bold leading-6 text-base">
+        <p className="font-title text-wheat font-bold leading-6 text-base">
           {buttonText}
         </p>
       </button>
       {/* modal__form-submit__divider */}
-      <div className="absolute  w-60 h-16 rounded-2xl border-4 border-[#D0564F] top-2"></div>
+      <div className="absolute  w-60 h-16 rounded-2xl border-4 border-wetCoral top-2"></div>
     </div>
   );
 };
