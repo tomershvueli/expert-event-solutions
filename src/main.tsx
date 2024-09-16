81f6d41224bfa502128ae5276d652b23c5d4644e
RuvenPinkhasov <rubi00010@gmail.com>
2024-09-16 14:33:15 -0400

    fix: fixed main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./global/default.css";
-import App from "./components/App/App";
 
 const client = new ApolloClient({
   uri: "http://localhost:8080/api/graphql",
@@ -14,7 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <BrowserRouter>
-          <App/>
+        <Routes>
+          <Route path="/" element={<div>Home Route</div>} />
+        </Routes>
       </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
