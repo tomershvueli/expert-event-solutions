139d339a76b58b035e7eaf50d06508289bbf3726
michaela budde <michaelabudde87@gmail.com>
2024-09-16 09:49:53 -0700

    feat: reverted main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./global/default.css";
-import { FormPhoneInputComponent } from "./components/Form/FormTextInputComponent";
 
 const client = new ApolloClient({
   uri: "http://localhost:8080/api/graphql",
@@ -15,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ApolloProvider client={client}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<FormPhoneInputComponent />} />
+          <Route path="/" element={<div>Home Route</div>} />
         </Routes>
       </BrowserRouter>
     </ApolloProvider>
