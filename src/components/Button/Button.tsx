ec7880161296b8755c950fb61bc5c7ee5c7e9652
RuvenPinkhasov <rubi00010@gmail.com>
2024-09-17 12:18:06 -0400

    fix: fixed location of lip on mobile screens

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -12,7 +12,7 @@ export const Button = ({ onClick, buttonText, submitButton }: Props) => {
         type={submitButton ? "submit" : "button"}
         onClick={onClick}
       >
-        <div className="group-hover:hidden absolute w-80 h-20 rounded-3xl border-[6px] border-wetCoral top-[13px] md:border-4 md:rounded-2xl md:w-56 md:h-14 md:top-2"></div>
+        <div className="group-hover:hidden absolute w-80 h-20 rounded-3xl border-[6px] border-wetCoral top-[13px] md:border-4 md:rounded-2xl md:w-56 md:h-14 md:left-[-4px] md:top-1"></div>
         <p
           className={`font-title text-wheat font-bold leading-8 text-button-lg md:text-button-sm md:leading-5`}
         >
