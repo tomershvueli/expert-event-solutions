89e90dde9c4b79b125c156b1e4e0eaefe702a1c6
RuvenPinkhasov <rubi00010@gmail.com>
2024-09-17 17:46:58 -0400

    feat: added components to the page

--- src/components/StaffingServices/StaffingServices.tsx
+++ src/components/StaffingServices/StaffingServices.tsx
@@ -1,16 +1,63 @@
+import { useState, useEffect } from "react";
 import { Header } from "../Header/Header";
 import { Image } from "../Image/Image";
 import { Footer } from "../Footer/Footer";
+import outdoor from "../../assets/image-outdoor1.png";
 
-export function StaffingServices() {
+interface Props {
+  screenWidth?: "mobile" | "desktop";
+}
+
+export function StaffingServices({ screenWidth }: Props) {
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+  const isMobile = screenWidth === "mobile";
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      setWindowWidth(width);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
       <Header />
-      <Image
-        src="src/assets/image-staffing.png"
-        alt="Staffing Services"
-        context="staffing"
-      />
+      {isMobile && windowWidth}
+      <div className="bg-wheat relative">
+        <h1 className="mt-[142px] tracking-tighter text-welcome-title font-title text-center text-wetCoral leading-[86px] font-bold pb-[80px]">
+          Staffing Services
+        </h1>
+        <div className="flex justify-center">
+          <Image src={outdoor} alt="Staffing Services" context="staffing" />
+        </div>
+        <p className="tracking-tight pt-[80px] max-w-[782px] text-center mx-auto px-[16px] font-text text-paragraph-lg leading-[28px] font-normal text-lightBlack">
+          Expert Event Solutions offers a variety of services to meet you needs.
+          Our team is experienced in planning, preparation, execution, customer
+          service, clean up and post event support for events ranging from
+          concerts, to art festivals, farmers markets, running and triathlon
+          events, weddings, corporate events and family functions.
+        </p>
+        <p className="tracking-tight pt-[40px] max-w-[782px] text-center mx-auto px-[16px] font-text text-paragraph-lg leading-[28px] font-normal text-lightBlack">
+          Let us help you with your event. We can custom design a package to
+          provide you with as much support as you need from planning, and
+          permitting to staffing to ordering supplies or coordinating rentals.
+          We are here to help and love to see our supported events come off
+          without a hitch. Let us bring our experience to the table and help you
+          make the most of your event.
+        </p>
+        <div></div>
+        <div></div>
+        <h2>Tell us about your event</h2>
+        <p>
+          Send the Team a Note. Tell us a little more about what you are looking
+          for.
+        </p>
+      </div>
       <Footer />
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./global/default.css";
+
+import { StaffingServices } from "./components/StaffingServices/StaffingServices";
 
 const client = new ApolloClient({
   uri: "http://localhost:8080/api/graphql",
@@ -13,9 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<div>Home Route</div>} />
-        </Routes>
+        <StaffingServices screenWidth="desktop" />
       </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
