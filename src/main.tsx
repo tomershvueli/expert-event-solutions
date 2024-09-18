8e75fd491dc9ff59fd28b676d165a2c8ece24bb2
jmjcoding <jmjcoding@icloud.com>
2024-09-18 12:22:30 -0500

    remove changes to main per pier review

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./global/default.css";
-import { ModalProvider } from "./components/ModalContext/ModalContext";
-import { ModalTrigger } from "./components/ModalContext/ModalTrigger";
+
 const client = new ApolloClient({
   uri: "http://localhost:8080/api/graphql",
   cache: new InMemoryCache(),
@@ -15,14 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ApolloProvider client={client}>
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <ModalProvider>
-                <ModalTrigger name="modal" children={<div></div>} />
-              </ModalProvider>
-            }
-          />
+          <Route path="/" element={<div>Home Route</div>} />
         </Routes>
       </BrowserRouter>
     </ApolloProvider>
