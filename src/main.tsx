fa4b86296fdd741ad9b2cb8f80cbdbdfbf9e5efe
RuvenPinkhasov <rubi00010@gmail.com>
2024-09-19 17:38:20 -0400

    feat: added a Route on main for the staff services page

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./global/default.css";
-
 import { StaffingServices } from "./components/StaffingServices/StaffingServices";
 
 const client = new ApolloClient({
@@ -15,7 +14,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <BrowserRouter>
-        <StaffingServices screenWidth="desktop" />
+        <Routes>
+          <Route path="/" element={<div>Home Route</div>} />
+          <Route path="/StaffingServices" element={<StaffingServices />} />
+        </Routes>
       </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
