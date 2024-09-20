8e48914eb896d84fae5842db978456fbdffba0ac
jmjcoding <jmjcoding@icloud.com>
2024-09-20 16:36:50 -0500

    clean up main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./global/default.css";
-// import { HorizontalListImage } from "./components/HorizontalList/HorizontalListImage/HorizontalListImage";
-import { HorizontalListHome } from "./components/HorizontalList/HorizontalListHome/HorizontalListHome";
-// import { HorizontalListStaffing } from "./components/HorizontalList/HorizontalListStaffing/HorizontalListStaffing";
 
 const client = new ApolloClient({
   uri: "http://localhost:8080/api/graphql",
@@ -16,9 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <BrowserRouter>
-        {/* <HorizontalListImage /> */}
-        <HorizontalListHome />
-        {/* <HorizontalListStaffing /> */}
+        <Routes>
+          <Route path="/" element={<div>Home Route</div>} />
+        </Routes>
       </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
