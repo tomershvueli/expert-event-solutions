79764cf21db20735043eab96e82669bf0b0f8db1
jmjcoding <jmjcoding@icloud.com>
2024-09-20 13:53:40 -0500

    add styles for responsiveness; add flex-wrap;

--- src/components/HorizontalList/HorizontalListStaffing/HorizontalListStaffing.tsx
+++ src/components/HorizontalList/HorizontalListStaffing/HorizontalListStaffing.tsx
@@ -112,9 +112,13 @@ export function HorizontalListStaffing() {
   return (
     <div>
       <HorizontalList
-        containerClassName="bg-wheat"
+        containerClassName="flex bg-wheat justify-center"
         unorderedListClassName="bg-lightGingerFlower flex 
-        justify-around h-[460px] py-[80px] px-[24px] rounded-[100px] sm:flex-col sm:h-[100%] sm:flex-col sm:justify-center
+        justify-around h-[100%] py-[80px] px-[24px] rounded-[100px] w-[95%] flex-wrap
+        lg:align-center lg:flex-col lg:w-[80%] 
+        md:flex-col md:justify-center md:py-[60px] md:px-[0px] md:rounded-[60px]
+        md:h-[100%] 
+        sm:flex-col sm:h-[100%] sm:flex-col sm:justify-center sm:w-[95%]
         sm:items-center sm:py-[60px] sm:px-[0px] sm:rounded-[60px]"
       >
         {staffingItems.map((item) => (
@@ -122,9 +126,12 @@ export function HorizontalListStaffing() {
             title={item.title}
             description={item.description}
             listClassName=" flex flex-col
-            text-center tracking-[-0.02em] py-[24px] px-[24px] gap-[24px] sm:px-[12px] sm:gap-[32px]"
-            titleClassName="font-title text-section-subtext text-wheat leading-[38.4px] sm:text-[28px]"
-            descriptionClassName="font-roboto text-[22px] text-wheat leading-[26.4px] sm:text-paragraph-sm"
+            text-center tracking-[-0.02em] py-[24px] px-[24px] gap-[24px]
+            sm:px-[12px] sm:gap-[32px]"
+            titleClassName="font-title text-section-subtext text-wheat leading-[38.4px] 
+            sm:text-[28px]"
+            descriptionClassName="font-roboto text-[22px] text-wheat leading-[26.4px]
+            sm:text-paragraph-sm"
           />
         ))}
       </HorizontalList>
