2ea44cc93faa1c2ce39e8bd2956a120f52c2923f
jmjcoding <jmjcoding@icloud.com>
2024-09-20 12:04:50 -0500

    update stories component to use imported HorizontalListStaffing

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./global/default.css";
-import { HorizontalListImage } from "./components/HorizontalList/HorizontalListImage/HorizontalListImage";
+// import { HorizontalListImage } from "./components/HorizontalList/HorizontalListImage/HorizontalListImage";
 // import { HorizontalListHome } from "./components/HorizontalList/HorizontalListHome/HorizontalListHome";
-// import { HorizontalListStaffing } from "./components/HorizontalList/HorizontalListStaffing/HorizontalListStaffing";
+import { HorizontalListStaffing } from "./components/HorizontalList/HorizontalListStaffing/HorizontalListStaffing";
 
 const client = new ApolloClient({
   uri: "http://localhost:8080/api/graphql",
@@ -16,9 +16,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <BrowserRouter>
-        <HorizontalListImage />
+        {/* <HorizontalListImage /> */}
         {/* <HorizontalListHome /> */}
-        {/* <HorizontalListStaffing /> */}
+        <HorizontalListStaffing />
       </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
